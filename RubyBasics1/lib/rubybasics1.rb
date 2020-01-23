53d82d3c25588ce252490c24d507421717635f7f
Ryan Fritz <rfritz2@uncc.edu>
2020-01-23 01:31:41 +0000

    RubyBasics1 - Part1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,19 @@
+# Lab 1
+# Part I
+def sum arr
+  total=0
+  arr.each do |number|
+    total +=number
+  end
+  return total
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
