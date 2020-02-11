c1593d98cdd4a26e738031fba1537a88f410c8dd
Ryan Fritz <rfritz2@uncc.edu>
2020-02-11 06:20:49 +0000

    Lab 9: Fun with words - Anagram Groups

--- ruby_calisthenics1/lib/fun_with_strings.rb
+++ ruby_calisthenics1/lib/fun_with_strings.rb
@@ -3,6 +3,7 @@ module FunWithStrings
     self.gsub(/[^a-zA-Z]/,'').downcase == self.gsub(/[^a-zA-z]/,'').downcase.reverse  end
   def count_words
     sentence = Hash.new
+    
     self.gsub(/[^a-zA-z\s]/,'').downcase.split.each do |word|
       if sentence[word] != nil
         sentence[word]+=1
@@ -13,7 +14,22 @@ module FunWithStrings
     sentence
   end
   def anagram_groups
-    # your code here
+    words = self.split
+    arr1 = Array.new
+    
+    words.each_with_index do |word, index|
+      unless arr1.any? { |array| array.include?(word) }
+        arr2 = Array.new
+        arr2.push(word)
+        words[index+1..-1].each do |word2|
+          if word.downcase.chars.sort.join == word2.downcase.chars.sort.join
+            arr2.push(word2)
+          end
+        end
+        arr1.push(arr2)
+      end
+    end
+    arr1
   end
 end
 
